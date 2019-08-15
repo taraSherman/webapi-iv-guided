d56eef677eb515e0536e9295349d7cb42178101c
Tara Sherman <taradelin@gmail.com>
2019-08-14 21:38:00 -0400

    initial commit

--- api/server.js
+++ api/server.js
@@ -1,33 +1,35 @@
 const express = require('express');
 const helmet = require('helmet');
 
-const Shoutouts = require('../data/shoutouts-model.js');
+const db = require('../data/db.js');
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 
-server.get('/', (req, res) => {
-  Shoutouts.find()
-  .then(shoutouts => {
-    res.status(200).json(shoutouts);
-  })
-  .catch (error => {
+server.get('/', async (req, res) => {
+  try {
+    const shoutouts = await db('shoutouts');
+    const messageOfTheDay = process.env.MOTD || 'Hello World!';
+    res.status(200).json({ motd: messageOfTheDay, shoutouts });
+  } catch (error) {
     console.error('\nERROR', error);
     res.status(500).json({ error: 'Cannot retrieve the shoutouts' });
-  });
+  }
 });
 
-server.post('/', (req, res) => {
-  Shoutouts.add(req.body)
-  .then(shoutout => {
-    res.status(201).json(shoutout);
-  })
-  .catch (error => {
+server.post('/', async (req, res) => {
+  try {
+    const [id] = await db('shoutouts').insert(req.body);
+    const shoutouts = await db('shoutouts');
+
+    const messageOfTheDay = process.env.MOTD || 'Hello World!';
+    res.status(201).json({ motd: messageOfTheDay, shoutouts });
+  } catch (error) {
     console.error('\nERROR', error);
     res.status(500).json({ error: 'Cannot add the shoutout' });
-  });
+  }
 });
 
 module.exports = server;
